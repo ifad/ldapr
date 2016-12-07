1f5d614d7d2e1555f6787b64420c06bf81a46909
Thimios Dimopulos <thimios@wantedpixel.com>
2016-12-07 18:09:06 +0100

    remove ifad specific logic

--- lib/ldapr/api.rb
+++ lib/ldapr/api.rb
@@ -1,8 +1,6 @@
 module LDAPR
   module API
     require 'ldap_model'
-    require_relative 'external/external_person'
-    require_relative 'ifad/ifad_person'
     require_relative 'api/v1'
   end
 end
