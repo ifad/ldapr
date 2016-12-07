1c316bc2cf1d1dfd0f7c4ce477c164f51cb0dddc
Thimios Dimopulos <thimios@wantedpixel.com>
2016-12-07 18:11:00 +0100

    remove reference to activerecord

--- lib/ldapr/api/v1/defaults.rb
+++ lib/ldapr/api/v1/defaults.rb
@@ -12,11 +12,6 @@ module LDAPR
             version 'v1'
             format :json
 
-            # global handler for simple not found case
-            rescue_from ::ActiveRecord::RecordNotFound do |e|
-              error_response(message: e.message, status: 404)
-            end
-
             # global exception handler, used for error notifications
             rescue_from :all do |e|
               LDAPR.logger.error(e)
