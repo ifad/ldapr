bd68f0458b50c9ac84cf3e7858bf542ac635fc6e
Thimios Dimopulos <thimios@wantedpixel.com>
2016-12-23 11:24:16 +0100

    add swagger and swaggerUi for documentation

--- lib/ldapr/application.rb
+++ lib/ldapr/application.rb
@@ -1,10 +1,11 @@
 require 'grape'
+require 'grape-swagger'
 require_relative 'api'
-
-require 'byebug' if development?
 
 module LDAPR
   class Application < Grape::API
     mount API::V1::Entry
+
+    add_swagger_documentation(base_path: "/", api_version:'v1', hide_documentation_path: true)
   end
 end
