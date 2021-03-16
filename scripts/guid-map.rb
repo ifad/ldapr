3d5d162dc9863865d8774e2e4559c88991d786ca
Marcello Barnaba <vjt@openssl.it>
2021-03-16 22:44:10 +0100

    fetch guid map from the whole directory

--- scripts/guid-map.rb
+++ scripts/guid-map.rb
@@ -8,7 +8,7 @@ filter &= Net::LDAP::Filter.eq('objectClass', 'user')
 
 people = LDAP.connection.search(
   attributes: %w( sAMAccountName objectGUID ),
-  base:   'ou=people,dc=ifad,dc=org',
+  base:   'dc=ifad,dc=org',
   filter: filter,
   scope:  Net::LDAP::SearchScope_WholeSubtree
 ).map {|entry| LDAP::Person.new(entry) }
